94bfb88c27ed226a7c3f0ae2c3dd095c55c4e69d
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-05-15 06:16:04 +0100

    Delete routes directory

--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup, signin, resetPassword } = require('./auth.controller');
-
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.post('/reset-password', resetPassword);
-
-module.exports = router;
